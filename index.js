e52f180728a0e0d83c337f6c7cff71beba5dca2c
Angie Rodriguez <angrodri902@gmail.com>
2020-01-19 19:55:24 -0800

    Update styles and sticky navbar

--- index.js
+++ index.js
@@ -35,3 +35,16 @@ card.addEventListener('mouseleave', () => {
         backCardText.appendChild(engText);
     }, 1000);
 });
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
